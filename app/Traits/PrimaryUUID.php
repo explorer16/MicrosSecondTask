86f5030f4cfab89294486aa69965d457bea481aa
Explorer16 <asadullaevdonier@gmail.com>
2025-10-23 21:13:07 +0500

    Fixed trait

--- app/Traits/PrimaryUUID.php
+++ app/Traits/PrimaryUUID.php
@@ -7,17 +7,20 @@ use Illuminate\Support\Str;
 
 trait PrimaryUUID
 {
-    public $incrementing = false;
-    protected $keyType = 'string';
-
     protected static function bootPrimaryUUID()
     {
-        parent::boot();
-
         static::creating(function ($model) {
             if (empty($model->{$model->getKeyName()})) {
                 $model->{$model->getKeyName()} = (string) Str::uuid();
             }
+        });
+    }
+
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            $model->incrementing = false;
+            $model->keyType = 'string';
         });
     }
 }
