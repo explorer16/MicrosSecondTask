838718354f02b6a52048fc4f036ad7b8acc70442
Explorer16 <asadullaevdonier@gmail.com>
2025-10-24 22:35:21 +0500

    Added login and register routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/login', function () {
-    return view('login');
-});
+    return view('auth.login');
+})->name('login');
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/register', function () {
-    return view('register');
-});
+    return view('auth.register');
+})->name('register');
+
+Route::post('/register', [AuthController::class, 'register']);
