c3dc2d106c5699c8ef65567e7eb57ec100ef9de9
Explorer16 <asadullaevdonier@gmail.com>
2025-10-24 22:34:08 +0500

    Added user repositories

---
+++ app/Repositories/Eloquent/UserRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\User;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use Illuminate\Support\Facades\Hash;
+
+class UserRepository implements UserRepositoryInterface
+{
+    public function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+}
