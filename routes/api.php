6b9729ec2b484f7bedf8c12740c11f680c21f587
Explorer16 <asadullaevdonier@gmail.com>
2025-10-25 12:24:09 +0500

    Added api routes

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'api',
+    'namespace' => 'App\Http\Controllers\Api',
+], function () {
+    Route::apiResource('suppliers', 'SupplierController');
+    Route::apiResource('categories', 'CategoryController');
+    Route::apiResource('products', 'ProductController');
+    Route::post('/products/upload', 'ProductController@upload');
+    Route::post('/products/import', 'ProductController@import');
+    Route::get('/changes', 'HistoryController@index');
+});
