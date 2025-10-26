ddd91a8c54644c00ec1452f11f8f911dc730b63f
Explorer16 <asadullaevdonier@gmail.com>
2025-10-26 13:12:38 +0500

    HasCreatedBy hot fix

--- app/Traits/HasCreatedBy.php
+++ app/Traits/HasCreatedBy.php
@@ -9,7 +9,7 @@ trait HasCreatedBy
     protected static function bootHasCreatedBy(): void
     {
         static::creating(function ($model) {
-            if (!$model->created_by) {
+            if ($model->created_by === null) {
                 $model->created_by = Auth::id();
             }
         });
